4f3bed4b6fa302093d38e8c60ba61a42cd2857ab
Heracles <andres.yepes419@pascualbravo.edu.co>
2024-12-27 15:16:15 -0500

    Act inicial
    Actualización para hacer que GitHub permita habilitar Pages

---
+++ js/scriptIndex.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArAy-x5JDjbK4GBQ_68jdolWp64OU3CCM",
+  authDomain: "programadorhorarios.firebaseapp.com",
+  projectId: "programadorhorarios",
+  storageBucket: "programadorhorarios.firebasestorage.app",
+  messagingSenderId: "717956155075",
+  appId: "1:717956155075:web:c8205138f79832bc35c476"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
